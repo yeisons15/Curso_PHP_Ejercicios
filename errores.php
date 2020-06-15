6ceac50e045456707dfd7a114c0ddb45494c50e1
Yeison Rojas <yeisons15@hotmail.com>
2020-06-14 22:24:30 -0500

    Se crea el archivo errorres.php | Desactivar notificaciones de errores y Guardar errores en un archivo .log

---
+++ errores.php
@@ -0,0 +1,20 @@
+
+<?php
+## Desactivar toda notificación de error
+error_reporting('E_ERROR');
+$nombre = 'Hola';
+
+echo nombre;
+echo nombres;
+
+## E_NOTICE informa de variabl
+
+
+
+## Guardar errores en un log
+ini_set("log_errores", 1);
+ini_set("error_log", "/tmp/php-error.log");
+error_log("Hay un error");
+
+
+?>
