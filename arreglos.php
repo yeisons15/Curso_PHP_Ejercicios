5a9c83f1d5cc3a581d63d5c7d877259093919e03
Yeison Rojas <yeisons15@hotmail.com>
2020-06-14 21:54:29 -0500

    Se trabaja, ordenamiento, ope aritmeticas, dividir, unir y eliminar elementos

--- arreglos.php
+++ arreglos.php
@@ -15,8 +15,8 @@
 #Arreglos
 
 // Los arreglos se declaran de las siguientes 2 formas
-$numeros = [];
-$numeros = array();
+//$numeros = [];
+//$numeros = array();
 
 // Para imprimir los arreglos usamos la siguente instrucción.
 
@@ -108,7 +108,7 @@ $arreglo = [];
 $videojuevos = ['FIFA','Fortnite','Red Deal', 'Call Of Duty', 'Battelfield','Pokemon', 'GTA','The Sims'];
 
 # saber si un arraglo está vacío - empty
-var_dump(empty($arreglo));
+/*var_dump(empty($arreglo));
 echo "<br>";
 var_dump(empty($videojuevos));
 echo "<br>";
@@ -124,6 +124,70 @@ echo count($videojuevos);
 # Antepenultimo elemento
 $posicion = count($videojuevos) - 2;
 echo $videojuevos[$posicion]." - ";
+*/
+
+## Ordenar arreglo alfabeticamente || Algo a considerar es que el arreglo pierde su indice al hacerlo de esta forma
+//sort($videojuevos);
+//var_dump($videojuevos);
+//echo "<br>";
+
+## Ordenar arreglo de manera inversa sin perder el indice
+//asort($videojuevos);
+//var_dump($videojuevos);
+
+## Ordernar de manera inversa (mayor a menor, o Z-A) || Algo a considerar es que el arreglo pierde su indice al hacerlo de esta forma
+//rsort($videojuevos);
+//var_dump($videojuevos);
+
+## Ordenar arreglo de manera inversa sin perder el indice
+//arsort($videojuevos);
+//var_dump($videojuevos);
+
+## Sumar valores del arreglo
+//$numeros = [1, 2, 10, 20];
+//$suma_arreglo = array_sum($numeros);
+//echo "La suma del arreglo es: ".$suma_arreglo;
+
+## Encontrar diferencias entre arreglos
+//$claseA=['a1'=>'Yeison', 'a2'=>'Juan','a3'=>'Amanda'];
+//$claseB=['a1'=>'Yeison', 'a2'=>'Juan','a3'=>'Amanda','a4'=>'Mariana','a5'=>'Salomon'];
+//$diferencia = array_diff($claseB, $claseA);
+//var_dump($diferencia);
+
+
+## Dividir un arreglo
+$videojuevos = ['FIFA','Fortnite','Red Deal', 'Call Of Duty', 'Battelfield','Pokemon', 'GTA','The Sims'];
+
+$dividir = array_chunk($videojuevos,2);
+//var_dump($dividir);
+
+## Dividir un arreglo y eliminar lo anterior
+//var_dump(array_slice($videojuevos,2));
+
+## Unir arreglos
+$frutas =['Fresa', 'Uva', 'Mora'];
+$verduras =['zanahoria', 'lechuga', 'coliflor'];
+
+$unir = array_merge($frutas,$verduras);
+//var_dump($unir);
+
+
+## Eliminar último elemento de un arreglo
+array_pop($videojuevos);
+var_dump($videojuevos);
+
+## Agregar un elemento al final del arreglo
+array_push($videojuevos,'Mario Bros');
+var_dump($videojuevos);
+
+
+## Buscar un elemento en un arreglo || Regresa la posición del primer valor encontrado
+$calAlumnos = array(10, 1, 3,3, 4);
+$buscar = array_search(3,$calAlumnos);
+var_dump($buscar);
+
+
+
 
 
 
