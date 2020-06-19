6c6239bd6acd70c0c101ff70b9cac7a83667c451
Yeison Rojas <yeisons15@hotmail.com>
2020-06-18 22:34:57 -0500

    Actualización archivo funciones.php

--- funciones.php
+++ funciones.php
@@ -1,6 +1,44 @@
 <?php
 
-    
+ function sumarNumeros($a, $b, $imprime = false){
+    $suma = $a + $b;
+    if ($imprime == true) {
+        echo "La suma es ".$suma . "<br />";
+    }else{
+        return $suma;
+    }
+ }   
+
+$numero1 = 30;
+$sumarNumeros($numero1;10;true);
+
+## Funcion Anonima
+
+$despedida = function(){
+    echo "Última Clase de PHP";
+};
+
+## Para llamar la función es:
+$despedida();
+
+
+## Función Closure
+
+function finalizaCurso(Closure $curso, $nombre){
+    return $curso($nombre);
+}   
+
+$php = function($nombre){
+    return "<p> Gracias" . $nombre . "Finaliza el curso";
+};
+
+$laravel = function($nombre){
+    return "<p> ". $nombre . "Finaliza el curso";
+}
+
+echo finalizaCurso($php, 'Juan');
+echo finalizaCurso($laravel, 'Juan');
+
 
 
 
