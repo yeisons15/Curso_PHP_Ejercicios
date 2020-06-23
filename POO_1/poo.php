3c0777c84c274022c6f54fb3db0cae7a0f97c2c8
Yeison Rojas <yeisons15@hotmail.com>
2020-06-22 20:36:42 -0500

    Se trabajan conceptos de Clase, Objeto y Método

--- POO_1/poo.php
+++ POO_1/poo.php
@@ -8,8 +8,14 @@ class Curso{
     public $moneda;
     public $profesor;
     public $disponible;
+
+    public function imprimirInfo(){
+        return "El nombre del curso es <Strong>" . $this->nombre . " </Strong>y quien imparte el curso es: " . $this->profesor . "<br />";
+    }
+
 }
 
+## Creación de Objeto o instancia 1 
 $php = new Curso();
 $php->nombre = 'POO en PHP';
 $php->duracion = '3 sesiones';
@@ -18,7 +24,23 @@ $php->moneda = 'USD';
 $php->profesor = 'Yesi Days';
 $php->disponible = true;
 
-var_dump($php);
+
+echo $php->imprimirInfo();
+
+//var_dump($php);
+
+## Creación de Objeto o instancia 2
+$javascript = new Curso();
+$javascript->nombre = 'JavaScript desde 0';
+$javascript->duracion = '6 sesiones';
+$javascript->costo = 0;
+$javascript->moneda = 'USD';
+$javascript->profesor = 'Álvaro Felipe';
+$javascript->disponible = true;
+
+echo $javascript->imprimirInfo();
+//var_dump($javascript);
+
 
 
 ?>
