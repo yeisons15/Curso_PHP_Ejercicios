6b50fab44f7522f7d2972165a448c241964784f2
Yeison Rojas <yeisons15@hotmail.com>
2020-06-23 21:26:03 -0500

    Se trabajan conceptos de acceso a métodos (public, private, protected)

---
+++ POO_1/Clase_2/includes/Curso.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+class Curso{
+
+    private $titulo;
+    private $profesor;
+    private $duracion:
+    private $costo;
+    private $disponible;
+
+    ## Creación del constructor
+    public function __construct ($titulo, $profesor, $duracion, $costo, $disponible){
+        $this->titulo = $titulo;
+        $this->profesor = $profesor;
+        $this->duracion = $duracion;
+        $this->costo = $costo;
+        $this->disponible = $disponible;
+
+    }
+
+    ## Encapsulación
+    ## Se usan los métodos getter (objener) y setter (asignar datos)
+    
+    $
+    public function otenerTitulo(){
+        return $this ->titulo;
+    }
+
+    public function obtenerProfesor(){
+        return $this ->profesor;
+    }
+
+    
+}
+
+
+
+?>
--- POO_1/Clase_2/index.php
+++ POO_1/Clase_2/index.php
@@ -7,5 +7,17 @@
 </head>
 <body>
     
+    <h1>POO Avanzado I</h1>
+<?php
+    define ('INC', '/includes/');
+    require_once __DIR__ . INC . 'Curso.inc.php';
+    require_once __DIR__ . INC . 'Estudiante.inc.php';
+
+    $curso1 = new Curso('POO en PHP', 'Yesy Days', '3 Sesiones', 10, true);
+    $curso2 = new Curso('JavaScript', 'Álvaro Felipe', '6 Sesiones', 12, true);
+    $curso3 = new Curso('BD Desde Cero', 'Alexys Lozada', '5 Sesiones', 12, true);
+
+?>
+
 </body>
 </html>
